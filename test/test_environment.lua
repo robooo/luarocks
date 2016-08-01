bc567cddce01ac887bacac81b637ec14f1d404e1
roboo <robo.karasek@gmail.com>
2016-08-01 15:43:18 +0200

    Appveyor preparation

--- test/test_environment.lua
+++ test/test_environment.lua
@@ -615,6 +615,7 @@ local function install_luarocks(install_env_vars)
    local testing_paths = test_env.testing_paths
    title("Installing LuaRocks")
    if test_env.TEST_TARGET_OS == "windows" then
+      print("BEFORE INSTALL.BAT")
       assert(execute_bool("install.bat /LUA " .. testing_paths.luadir .. " /P " .. testing_paths.testing_lrprefix .. " /NOREG /NOADMIN /F /Q /CONFIG " .. testing_paths.testing_dir, false, install_env_vars))
       print("AFTER INSTALL.BAT")
       assert(execute_bool(testing_paths.win_tools .. "/cp " .. testing_paths.testing_lrprefix .. "/lua/luarocks/site_config* "
