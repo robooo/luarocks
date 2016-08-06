de20576bdb75f40bdf4b6bed41ae0eb523d1f900
roboo <robo.karasek@gmail.com>
2016-08-06 16:33:41 +0200

    Appveyor fixing

--- test/test_environment.lua
+++ test/test_environment.lua
@@ -186,7 +186,7 @@ function test_env.remove_dir(path)
    path=path:gsub("/", "\\")
    print("EXISTS2 " .. path)
    print("CURRENT " .. lfs.currentdir())
-   print(exists(path:gsub("\\testing-5.1","")))
+   print(exists(lfs.currentdir()))
    if exists(path) then
 
       for file in lfs.dir(path) do
