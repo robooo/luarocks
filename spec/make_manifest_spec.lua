2d5b6b030f73ab3054ce501918bf0511e02e3257
roboo <robo.karasek@gmail.com>
2016-08-19 17:15:03 +0200

    Codecov try

--- spec/make_manifest_spec.lua
+++ spec/make_manifest_spec.lua
@@ -1,17 +1,17 @@
--- local test_env = require("test/test_environment")
--- local run = test_env.run
+local test_env = require("test/test_environment")
+local run = test_env.run
 
--- test_env.unload_luarocks()
+test_env.unload_luarocks()
 
--- describe("LuaRocks make_manifest tests #blackbox #b_make_manifest", function()
+describe("LuaRocks make_manifest tests #blackbox #b_make_manifest", function()
 
---    before_each(function()
---       test_env.setup_specs()
---    end)
+   before_each(function()
+      test_env.setup_specs()
+   end)
 
---    describe("LuaRocks-admin make manifest tests", function()
---       it("LuaRocks-admin make manifest", function()
---          assert.is_true(run.luarocks_admin_bool("make_manifest"))
---       end)
---    end)
--- end)
+   describe("LuaRocks-admin make manifest tests", function()
+      it("LuaRocks-admin make manifest", function()
+         assert.is_true(run.luarocks_admin_bool("make_manifest"))
+      end)
+   end)
+end)
