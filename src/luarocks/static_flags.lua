b9559f02187baa845d38eb3a9226aacdef84d522
daurnimator <quae@daurnimator.com>
2017-09-01 23:02:38 +1000

    luarocks.static_flags: Get paths for static libraries via repos.recurse_rock_manifest_tree

--- src/luarocks/static_flags.lua
+++ src/luarocks/static_flags.lua
@@ -3,10 +3,11 @@ local static_flags = {}
 package.loaded["luarocks.static_flags"] = static_flags
 
 local cfg = require("luarocks.cfg")
+local dir = require("luarocks.dir")
 local fetch = require("luarocks.fetch")
-local fs = require("luarocks.fs")
 local manif = require("luarocks.manif")
 local path = require("luarocks.path")
+local repos = require("luarocks.repos")
 local search = require("luarocks.search")
 local util = require("luarocks.util")
 
@@ -45,27 +46,32 @@ local function collect_libs(module_name, flags, libs_table)
    if not module_name then
       return nil, "Argument missing. "..util.see_help("static_flags")
    end
+   if libs_table == nil then
+      libs_table = {}
+   end
 
-   local name, version, repo, repo_url = search.pick_installed_rock(module_name:lower(), version, flags["tree"])
+   local name, version = search.pick_installed_rock(module_name:lower(), nil, flags["tree"])
    if not name then
       util.printout(name..(version and " "..version or "").." is not installed.")
       return nil, version
    end
-   assert(type(repo_url) == "string")
-   local manifest, err = manif.load_manifest(repo_url)
-   if not manifest then
+
+   -- Find static libraries for current module
+   local rock_manifest, err = manif.load_rock_manifest(name, version)
+   if not rock_manifest then
       return nil, err
    end
-
-   local libs_table = libs_table or {}
-   local rockspec = fetch.load_local_rockspec(path.rockspec_file(name, version), false)
-   local minfo = manifest.repository[name][version][1]
-   for lib_name in pairs(minfo.modules) do
-      if lib_name:match("luarocks-(.-)" .. util.matchquote(cfg.lib_static_extension) .. "$") then
-         table.insert(libs_table, cfg.home_tree..cfg.lib_modules_path..lib_name)
+   repos.recurse_rock_manifest_tree(rock_manifest.lib, function(parent_path, parent_module, file)
+      local file_path = parent_path .. file
+      if file:match("^luarocks-(.-)" .. util.matchquote(cfg.lib_static_extension) .. "$") then
+         local static_lib_path = dir.path(cfg.deploy_lib_dir, file_path)
+         table.insert(libs_table, static_lib_path)
       end
-   end
+      return true
+   end)
 
+   -- Add any external dependencies
+   local rockspec = fetch.load_local_rockspec(path.rockspec_file(name, version), false)
    if rockspec.external_dependencies then
       for _, desc in pairs(rockspec.external_dependencies) do
          if desc.library then
@@ -74,11 +80,13 @@ local function collect_libs(module_name, flags, libs_table)
       end
    end
 
+   -- Add luarocks dependencies of current module
    for _, dep in ipairs(rockspec.dependencies) do
       if not dep.name:match("lua$") then
          collect_libs(dep.name, flags, libs_table)
       end
    end
+
    return libs_table
 end
 
